c677a40f72eed54411bcc02dbd8cb65d721dfdf3
abdul diallo <rahimi.diallo96@gmail.com>
2019-12-12 14:20:56 -0500

    project initialized

---
+++ mortageCalc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class mortageCalc {
+
+    Scanner input;
+
+    public static void main (Streing[] args) {
+
+        input=new Scanner(System.in);
+
+    }
+
+}
