fff3ba030845820bf2a9ac8b89bb460a0fd5f875
abdul diallo <rahimi.diallo96@gmail.com>
2019-12-12 21:46:23 -0500

    updated

--- calc.java
+++ calc.java
@@ -6,14 +6,16 @@ public class calc {
     private int freqPayment;
     private double downPayment;
 
-    calc(double propertyPrice, double rate, int rate, int freqPayment) {
+    calc(double propertyPrice, double rate, int rate, int freqPayment, double downPayment) {
         this.propertyPrice = propertyPrice;
         this.year = year;
         this.freqPayment = freqPayment;
+        this.downPayment = downPayment;
     }
 
     public int mortage(){
         return propertyPrice-downPayment;
     }
+
 
 }
