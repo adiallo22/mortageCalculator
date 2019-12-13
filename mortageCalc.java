12746b1d954b9d7164468b636d69ee4d3f22e901
abdul diallo <rahimi.diallo96@gmail.com>
2019-12-12 21:47:31 -0500

    data inputed and object calc created

--- mortageCalc.java
+++ mortageCalc.java
@@ -1,16 +1,23 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class mortageCalc {
 
-    Scanner input;
+    public Scanner input;
 
-    public static void main (Streing[] args) {
+    public static void main (String[] args) {
 
         input=new Scanner(System.in);
+        System.out.println("Enter the price of the property : ");
+        double propertyPrice = input.nextDouble();
+        System.out.println("Enter the rate : ");
+        double rate = input.nextInt();
+        System.out.println("How many time a year you make payment : ");
+        int freqPayment = input.nextInt();
+        System.out.println("How much did you pay down? : ");
+        double downPayment = input.nextDouble();
 
-    }
-
-    public int mortage() {
+        calc serie - new calc(propertyPrice, rate, rate, freqPayment, downPayment);
 
     }
 
